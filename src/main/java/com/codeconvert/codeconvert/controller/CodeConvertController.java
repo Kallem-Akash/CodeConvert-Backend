1657b884c44abae5ebe8b8882a4c1ea807741546
KALLEM AKASH <akashgoud018@gmail.com>
2025-07-17 21:21:56 +0530

    error solved

--- src/main/java/com/codeconvert/codeconvert/controller/CodeConvertController.java
+++ src/main/java/com/codeconvert/codeconvert/controller/CodeConvertController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://codexconvert.vercel.app")
+@CrossOrigin("*")
 public class CodeConvertController {
 
     @Autowired
