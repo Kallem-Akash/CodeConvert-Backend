0d07d9ee19b4f4ffb8166b0db74d3e4bbeb4f477
KALLEM AKASH <akashgoud018@gmail.com>
2025-08-30 16:36:05 +0530

    added docker file

--- src/main/java/com/codeconvert/codeconvert/config/CorsConfig.java
+++ src/main/java/com/codeconvert/codeconvert/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5176") // Your frontend origin
+                .allowedOrigins("https://codexconvert.vercel.app") // Your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
